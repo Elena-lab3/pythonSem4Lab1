f5f2197d99f996a5619b868e509c4a927c5e11e7
LenaBar <barkovskaaelena3@gmail.com>
2021-04-24 22:35:15 +0300

    lab02

---
+++ lab01/hello.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+
+import logging
+
+logging.basicConfig(format="%(message)s", level=logging.INFO)
+
+
+def main():
+    logging.info("Hello! This is Info message!")
+
+
+if __name__ == "__main__":
+    try:
+        main()
+    except KeyboardInterrupt:
+        logging.info("Oh no!!!")
